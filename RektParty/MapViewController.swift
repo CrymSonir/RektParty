d0565d795212d1361eba385ad8bdb129410fb67a
Soleet <vincent.weyh@live.fr>
2017-03-20 16:34:51 +0100

    working on add button for google map and marker

--- RektParty/MapViewController.swift
+++ RektParty/MapViewController.swift
@@ -11,8 +11,11 @@ import GoogleMaps
 import GooglePlaces
 
 class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var addMarkerBtn: UIButton!
     
-    @IBOutlet var mapView: GMSMapView!
+    var markersList: [GMSMarker] = []
     
     var locationManager = CLLocationManager()
     var currentLocation: CLLocation?
@@ -28,6 +31,13 @@ class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManager
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        addMarkerBtn.contentVerticalAlignment = .center
+        
+        addMarkerBtn.frame = CGRect(x: 160, y: 100, width: 50, height: 50)
+        addMarkerBtn.layer.cornerRadius = 0.5 * addMarkerBtn.bounds.size.width
+        addMarkerBtn.clipsToBounds = true
+        view.addSubview(addMarkerBtn)
+        
         mapView.isMyLocationEnabled = true
         mapView.settings.myLocationButton = true
         mapView.delegate = self
@@ -42,6 +52,8 @@ class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManager
         
         self.locationManager.delegate = self
         self.locationManager.startUpdatingLocation()
+        
+        getMarkers()
     }
 
     override func didReceiveMemoryWarning() {
@@ -65,6 +77,31 @@ class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManager
         //Finally stop updating location otherwise it will come again and again in this delegate
         self.locationManager.stopUpdatingLocation()
         
+    }
+    
+    func getMarkers(){
+        // TODO GET CREATED MARKER BY USER
+        
+        var markerPosition = CLLocationCoordinate2DMake(45.770301, 4.86365)
+        var marker = GMSMarker(position: markerPosition)
+        marker.title = "Orgie a lyon"
+        marker.icon = UIImage(named: "marker")
+        markersList.append(marker)
+        marker.map = self.mapView
+        
+        markerPosition = CLLocationCoordinate2DMake(46.770301, 4.86365)
+        marker = GMSMarker(position: markerPosition)
+        marker.title = "Orgie a paris"
+        marker.icon = UIImage(named: "marker")
+        markersList.append(marker)
+        marker.map = self.mapView
+        
+        markerPosition = CLLocationCoordinate2DMake(47.770301, 4.86365)
+        marker = GMSMarker(position: markerPosition)
+        markersList.append(marker)
+        marker.title = "Orgie chez ta mere"
+        marker.icon = UIImage(named: "marker")
+        marker.map = self.mapView
     }
     
     /*
