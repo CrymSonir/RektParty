33b542cb71c99c68d053d5ed8b1c08613dd3e7d6
Soleet <vincent.weyh@live.fr>
2017-03-20 14:50:25 +0100

    Add MyLocation

---
+++ RektParty/MapViewController.swift
@@ -0,0 +1,80 @@
+//
+//  MapControllerViewController.swift
+//  RektParty
+//
+//  Created by stagiaire on 20/03/2017.
+//  Copyright © 2017 The Grilled Birds. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+import GooglePlaces
+
+class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet var mapView: GMSMapView!
+    
+    var locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    var placesClient: GMSPlacesClient!
+    var zoomLevel: Float = 15.0
+    
+    // An array to hold the list of likely places.
+    var likelyPlaces: [GMSPlace] = []
+    
+    // The currently selected place.
+    var selectedPlace: GMSPlace?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
+        mapView.delegate = self
+        
+        // The myLocation attribute of the mapView may be null
+        if let mylocation = mapView.myLocation {
+            print("User's location: \(mylocation)")
+            //Location Manager code to fetch current location
+        } else {
+            print("User's location is unknown")
+        }
+        
+        self.locationManager.delegate = self
+        self.locationManager.startUpdatingLocation()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //Location Manager delegates
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+
+        let location = locations.first
+        let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude:(location?.coordinate.longitude)!, zoom:14)
+        let markerPosition = CLLocationCoordinate2DMake((location?.coordinate.latitude)!, (location?.coordinate.longitude)!)
+        let marker = GMSMarker(position: markerPosition)
+        marker.title = "Me"
+        marker.map = mapView
+        marker.icon = UIImage(named: "marker")
+        mapView.selectedMarker = marker
+        mapView.animate(to: camera)
+        
+        //Finally stop updating location otherwise it will come again and again in this delegate
+        self.locationManager.stopUpdatingLocation()
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
