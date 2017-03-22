664c0aff4e21b1add427eea24c78b75439eae43b
ygout <yohann.goutaret@gmail.com>
2017-03-22 17:35:24 +0100

    Fix merge to master

--- client/RektParty/ProfileViewController.swift
+++ client/RektParty/ProfileViewController.swift
@@ -11,7 +11,7 @@ import Alamofire
 import JWT
 
 class ProfileViewController: UIViewController {
-
+    
     @IBOutlet weak var txtNickName: UITextField!
     @IBOutlet weak var txtMail: UITextField!
     @IBOutlet weak var dpBirthDate: UIDatePicker!
@@ -32,19 +32,19 @@ class ProfileViewController: UIViewController {
         //print(userData["birthDate"]!)
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd/mm/yyyy" //Your date format
-        let dateFromated = dateFormatter.date(from: userData["birthDate"] as! String)
+        let dateFormated = dateFormatter.date(from: userData["birthDate"] as! String)
         //print(dateFromated!)
-        self.dpBirthDate.date = dateFromated!
-
+        self.dpBirthDate.date = dateFormated!
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     @IBAction func editProfile(_ sender: UIButton) {
         
         let db = UserDefaults.standard
@@ -55,54 +55,30 @@ class ProfileViewController: UIViewController {
         let dateString = dateFormatter.string(from: dpBirthDate.date)
         var parameters: Parameters;
         if txtPassword.text != "" {
-             parameters = ["mail": txtMail.text!, "password": txtPassword.text!,
-                                          "fisrtName": txtFirstName.text!,"lastName": txtLastName.text!,
-            "birthDate": dateString,"pseudo": txtNickName.text!,"token": token!];
+            parameters = ["mail": txtMail.text!, "password": txtPassword.text!,
+                          "firstName": txtFirstName.text!,"lastName": txtLastName.text!,
+                          "birthDate": dateString,"pseudo": txtNickName.text!,"token": token!];
             print("here")
         }else{
-             parameters = ["mail": txtMail.text!,
-                        "fisrtName": txtFirstName.text!,"lastName": txtLastName.text!,
-                        "birthDate": dateString,"pseudo": txtNickName.text!,"token":token!];
+            parameters = ["mail": txtMail.text!,
+                          "firstName": txtFirstName.text!,"lastName": txtLastName.text!,
+                          "birthDate": dateString,"pseudo": txtNickName.text!,"token":token!];
         }
-        print("===========================" + token!)
+        print(parameters)
         Alamofire.request("http://192.168.100.100:4567/user",method: .put, parameters: parameters).responseString { response in
             
             if let JSON = response.result.value {
                 print("JSON: \(JSON)")
                 do {
                     // let allo:String = JSON as! String
-
-                    
-                    let result = try JWT.decode(JSON, algorithm: .hs256("my$ecretK3y".data(using: .utf8)!))
-                    print(result)
-                    /*let db = UserDefaults.standard
-                    let userData = db.object(forKey: "userData") as! NSDictionary
-                   
-                    serData["mail"] = self.txtMail.text!*/
-                    /*userData["pseudo"] = self.txtFirstName.text!
-                    userData["firstName"] =self.txtFirstName.text!
-                    userData["lastName"] =self.txtFirstName.text!
-                    userData["birthDate"] = self.txtFirstName.text!
-
                     let db = UserDefaults.standard
                     let result = try JWT.decode(JSON, algorithm: .hs256("my$ecretK3y".data(using: .utf8)!))
                     print(result)
-
                     let userData = [
                         "pseudo":result["pseudo"],
                         "birthDate":result["birthDate"],
                         "lastName":result["lastName"],
                         "firstName":result["firstName"] ,
-
-                        "mail":result["mail"],
-                        "token": JSON
-                    ]
-                    
-                    db.set(true, forKey: "isLog")
-                    db.set(userData, forKey: "userData")
-                    self.dismiss(animated: true)*/
-                    self.viewDidLoad()
-
                         "mail":result["mail"]
                         
                     ]
@@ -119,14 +95,14 @@ class ProfileViewController: UIViewController {
         }
     }
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
-
+    
 }
